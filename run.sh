cc79bf57b7054052a94af33c5d3b183f811e02ea
Jie Zhou <jzhou41@cs.rochester.edu>
2021-10-15 21:59:09 -0400

    Updated run.sh to install or remind users to install docker if not exist
    Updated the script to first check if docker and docker-compose exist.
    It then guides users to install them if they do not exist.

--- run.sh
+++ run.sh
@@ -1,2 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#
+# This script (first downloads if not existed and) launches the docker image
+# for this tutoria.
+#
+
+DOCKER_INSTALL_CMD="sudo apt-get install docker-ce docker-ce-cli containerd.io"
+DOCKER_COMPOSE_INSTALL_CMD="curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
+
+#
+# Initialize the environment.
+#
+init() {
+    if [[ $1 == "install" ]]; then
+        install
+        exit
+    fi
+
+    # Check if docker is installed.
+    if [[ ! `which docker` ]]; then
+        echo "Please first install docker and docker-compose."
+        echo "On Linux, run "
+        echo "sudo $DOCKER_INSTALL_CMD"
+        echo "and "
+        echo "sudo $DOCKER_COMPOSE_INSTALL_CMD"
+        echo ""
+        echo "Or you can directly use this script to install docker by \"sudo ./run install\""
+
+        exit
+    fi
+
+    # Check if docker-compose is installed
+    if [[ ! `which docker-compose` ]]; then
+        echo "Please install docker-compose"
+        echo "On Linux, run "
+        echo "sudo $DOCKER_COMPOSE_INSTALL_CMD"
+        echo ""
+        echo "Or you can directly use this script to install docker-compose by \"sudo ./run install\""
+
+        exit
+    fi
+}
+
+#
+# Install docker and docker-compose
+#
+install() {
+    if [[ ! `which docker` ]]; then
+        $DOCKER_INSTALL_CMD
+    fi
+
+    if [[ ! `which docker-compose` ]]; then
+        sudo $DOCKER_COMPOSE_INSTALL_CMD
+        sudo chmod +x /usr/local/bin/docker-compose
+        exit
+    fi
+}
+
+#
+# Entrance of this script
+#
+init $1
+
+#
+# Launch the tutorial image.
+#
 docker-compose run tutorial bash
